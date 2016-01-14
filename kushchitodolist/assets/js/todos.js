62fc146ac19920ca087b70e536e510b9f7ed816f
Shlomi <shlomik@gmail.com>
2016-01-14 14:27:00 +0200

    removing todo item after dafing it out, because fade out is not removing

--- kushchitodolist/assets/js/todos.js
+++ kushchitodolist/assets/js/todos.js
@@ -7,6 +7,8 @@ $("li").click(function(){
 });
 
 $("span").click(function(event){
-    $(this).parent().fadeOut(600);
+    $(this).parent().fadeOut(600, function(){ // $(this) referes to the span element
+        $(this).remove(); // $(this) refers to the element currently fading out - which is the parent of span, or - the li element.
+    });
     event.stopPropagation();
 });
