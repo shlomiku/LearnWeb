e18f242af02a60776da3cf31bd67fe391d875f25
Shlomi <shlomik@gmail.com>
2016-01-14 15:03:18 +0200

    bugfix: strike out and delete didn't work for new elements
    the reason for that is when we do $("li").click() it referes only to already existing elements
    for new elements we need to use "on", and add it to the parent, telling it only to do something when "span" or "li" is clicked inside the "ul"

--- kushchitodolist/assets/js/todos.js
+++ kushchitodolist/assets/js/todos.js
@@ -2,11 +2,11 @@
  * Created by Admin on 14/01/2016.
  */
 // mark todo as completed
-$("li").click(function(){
+$("ul").on("click", "li", function(){
     $(this).toggleClass("completed");
 });
 
-$("span").click(function(event){
+$("ul").on("click", "span", function(event){
     $(this).parent().fadeOut(600, function(){ // $(this) referes to the span element
         $(this).remove(); // $(this) refers to the element currently fading out - which is the parent of span, or - the li element.
     });
