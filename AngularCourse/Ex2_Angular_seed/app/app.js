57d53022136e534fd89b1f72aab41e09df8cb9da
Shlomi <shlomik@gmail.com>
2016-01-25 12:10:12 +0200

    bugfix: bad routing in app.js

--- AngularCourse/Ex2_Angular_seed/app/app.js
+++ AngularCourse/Ex2_Angular_seed/app/app.js
@@ -1,12 +1,17 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('photoStore', [
+var app = angular.module('photoStore', [
   'ngRoute',
   'photoStore.view1',
   'photoStore.view2',
   'photoStore.templates'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/templates'});
+]);
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.
+    when('/', {
+      templateUrl: 'templates/templates.html',
+      controller: 'TemplatesCtrl'
+    }).
+    otherwise({redirectTo: '/templates'});
 }]);
