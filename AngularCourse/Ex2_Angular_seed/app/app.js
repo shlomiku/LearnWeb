a4ab322286a7aa91a3fe7f45fde7d68172c42182
Shlomi <shlomik@gmail.com>
2016-01-31 18:10:06 +0200

    LW-29 create controller and embed it template-details.html

--- AngularCourse/Ex2_Angular_seed/app/app.js
+++ AngularCourse/Ex2_Angular_seed/app/app.js
@@ -3,8 +3,6 @@
 // Declare app level module which depends on views, and components
 var app = angular.module('photoStore', [
   'ngRoute',
-  'photoStore.view1',
-  'photoStore.view2',
   'photoStore.templates'
 ]);
 app.config(['$routeProvider', function($routeProvider) {
