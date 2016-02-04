0750d02b99cc359784ed42e486344d5e460bfe11
Shlomi <shlomik@gmail.com>
2016-02-04 17:50:57 +0200

    static setup

--- AngularCourse/Ex3_django_contact_list/setup/settings.py
+++ AngularCourse/Ex3_django_contact_list/setup/settings.py
@@ -104,5 +104,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+STATIC_ROOT = os.path.join(BASE_DIR, "static_cdn")
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
--- AngularCourse/Ex3_django_contact_list/setup/urls.py
+++ AngularCourse/Ex3_django_contact_list/setup/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf.urls.static import static
 import contact_list
+import setup.settings as settings
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
