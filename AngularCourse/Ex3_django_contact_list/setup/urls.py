7fdfcda38cf20c826905cacc3cf64fe8c30fbc2e
Shlomi <shlomik@gmail.com>
2016-02-04 17:40:41 +0200

    adding support for static files.

--- AngularCourse/Ex3_django_contact_list/setup/urls.py
+++ AngularCourse/Ex3_django_contact_list/setup/urls.py
@@ -1,8 +1,13 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf.urls.static import static
 import contact_list
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('contact_list.urls')),
     ]
+
+
+if settings.DEBUG:
+    urlpatterns.append(static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
