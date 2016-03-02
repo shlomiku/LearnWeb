7ca7183df896f687c70a3dd74309ede416257c58
Shlomi <shlomik@gmail.com>
2016-03-02 21:19:27 +0200

    loading a component from another component

--- Angular2Course/dev/app.component.ts
+++ Angular2Course/dev/app.component.ts
@@ -1,11 +1,15 @@
 import {Component} from 'angular2/core';
+import {MyComponentComponent} from "./my-component.component";
 
 @Component({
     selector: 'my-app',
     template: `
         <h1>Angular 2 Boilerplate</h1>
         <p>Hello World!</p>
+        second component
+        <my-componet></my-componet>
     `,
+    directives: [MyComponentComponent]
 })
 export class AppComponent {
 
---
+++ Angular2Course/dev/my-component.component.ts
@@ -0,0 +1,15 @@
+/**
+ * Created by Admin on 02/03/2016.
+ */
+import {Component} from 'angular2/core'
+
+
+@Component({
+    selector: 'my-component',
+    template: `
+    This is my component!
+    `
+})
+export class MyComponentComponent {
+
+}
