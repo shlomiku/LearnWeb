ce3ec465806e0e7fa75310c49a3c19a42319631a
Shlomi <shlomik@gmail.com>
2016-03-11 20:17:15 +0200

    style.display method 1

--- Angular2Course/PuzzleApp/dev/puzzle/puzzle.component.ts
+++ Angular2Course/PuzzleApp/dev/puzzle/puzzle.component.ts
@@ -5,18 +5,18 @@ import {Component} from 'angular2/core';
     selector: 'my-puzzle',
     template:`
         <section class="setup">
-            Enter your name: <input type="text">
+            Enter your name: <input type="text" #name (keyup)="0">
         </section>
-        <section>
-        <h2>The Puzzle</h2>
-        <p>Welcome <span class="name">XXX</span></p>
-        <br>
-        Switch1: <input type="text"><br>
-        Switch2: <input type="text"><br>
-        Switch3: <input type="text"><br>
-        Switch4: <input type="text"><br>
+        <section [style.display]="name.value === '' ? 'none' : 'block'">
+            <h2>The Puzzle</h2>
+            <p>Welcome <span class="name">XXX</span></p>
+            <br>
+            Switch1: <input type="text"><br>
+            Switch2: <input type="text"><br>
+            Switch3: <input type="text"><br>
+            Switch4: <input type="text"><br>
+            <h4>Great {name.value} you did it!</h4>
         </section>
-        <h4>Great XXX you did it!</h4>
     `
 })
 export class PuzzleComponent{
