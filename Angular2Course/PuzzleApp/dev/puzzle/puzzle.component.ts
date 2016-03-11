7a0eeb4105408318b2001f46bd458857b0b02784
Shlomi <shlomik@gmail.com>
2016-03-11 19:58:47 +0200

    basuc styling

--- Angular2Course/PuzzleApp/dev/puzzle/puzzle.component.ts
+++ Angular2Course/PuzzleApp/dev/puzzle/puzzle.component.ts
@@ -4,7 +4,19 @@ import {Component} from 'angular2/core';
 @Component({
     selector: 'my-puzzle',
     template:`
-        Hi from shlomi
+        <section class="setup">
+            Enter your name: <input type="text">
+        </section>
+        <section>
+        <h2>The Puzzle</h2>
+        <p>Welcome <span class="name">XXX</span></p>
+        <br>
+        Switch1: <input type="text"><br>
+        Switch2: <input type="text"><br>
+        Switch3: <input type="text"><br>
+        Switch4: <input type="text"><br>
+        </section>
+        <h4>Great XXX you did it!</h4>
     `
 })
 export class PuzzleComponent{
