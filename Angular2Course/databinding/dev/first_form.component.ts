991c087b06e5cfb1901f81ce43a54caf82cc9b13
Shlomi Kushchi <shlomik@gmail.com>
2016-05-14 18:57:49 +0300

    adding event triger when first form is submitted

--- Angular2Course/databinding/dev/first_form.component.ts
+++ Angular2Course/databinding/dev/first_form.component.ts
@@ -2,6 +2,7 @@
  * Created by user on 5/12/2016.
  */
 import {Component} from "angular2/core";
+import {EventEmitter} from "angular2/core";
 @Component({
     selector: 'first-form',
     template: `
@@ -9,36 +10,38 @@ import {Component} from "angular2/core";
         <div class="my-box">
             Please Enter your name<input type="text" [(ngModel)]="myself.name" (keyup)="onKeyUp()"><br>
             Please Enter your age<input type="text" [(ngModel)]="myself.age" (keyup)="onKeyUp()"><br><br>
-            <button [disabled]="!isFilled">Submit</button>
-            {{isValid}}
-            <div>Your data is  Valid</div>
+            <button [disabled]="!isValid" (click)="onSubmit()">Submit</button>
+            <hr>
             <div>Your data is Filled: {{isFilled ? 'Yes' : 'No'}}</div>
-            {{ss}}
+            <div>Your data is  Valid: {{isValid ? 'Yes' : 'No'}}</div>
         </div>
     </div>
-    {{myself.name}}
     `,
-    // inputs: ['myself']
+    // inputs: ['myself'],
+    outputs: ['submitted']
 })
 export class FirstForm{
     myself = {name: '', age: ''};
+    // myself: any;
     isFilled = false;
     isValid = false;
-    ss = 'ss';
+    submittedEvent = new EventEmitter<{name: string, age: string}>();
 
     onKeyUp() {
-        if (this.myself.name != '' && this.myself.age != ''){
+        if (this.myself.name != '' && this.myself.age != '') {
             this.isFilled = true;
         }
         else {
             this.isFilled = false;
         }
-        if (this.myself.name != '' && /^\d+$/.test(this.myself.age)){
+        if (this.myself.name != '' && /^\d+$/.test(this.myself.age)) {
             this.isValid = true;
         }
         else {
             this.isValid = false;
         }
-}
-
+    };
+    onSubmit() {
+           this.submittedEvent.emit(this.myself);
+    };
 }
