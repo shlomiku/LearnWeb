07267013095f79117699629dc77484fdba2af4ab
Shlomi Kushchi <shlomik@gmail.com>
2016-07-15 23:59:45 +0300

    adding a template recipes

---
+++ Angular2Course/section3_recipe_book/src/app/recipes/recipes.component.spec.ts
@@ -0,0 +1,20 @@
+/* tslint:disable:no-unused-variable */
+
+import { By }           from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import {
+  beforeEach, beforeEachProviders,
+  describe, xdescribe,
+  expect, it, xit,
+  async, inject
+} from '@angular/core/testing';
+
+import { RecipesComponent } from './recipes.component';
+
+describe('Component: Recipes', () => {
+  it('should create an instance', () => {
+    let component = new RecipesComponent();
+    expect(component).toBeTruthy();
+  });
+});
---
+++ Angular2Course/section3_recipe_book/src/app/recipes/recipes.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'rb-recipes',
+  templateUrl: 'recipes.component.html',
+  styles: []
+})
+export class RecipesComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
