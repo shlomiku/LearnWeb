476592261aa8c94bdb1441e20f8ae3e35dd0fed0
Shlomi Kushchi <shlomik@gmail.com>
2016-07-15 23:51:28 +0300

    embedding the navcar in our page

--- Angular2Course/section3_recipe_book/src/app/app.component.ts
+++ Angular2Course/section3_recipe_book/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {HeaderComponent} from "./header.component";
 
 @Component({
   moduleId: module.id,
   selector: 'rb-root',
   templateUrl: 'app.component.html',
+  directives: [HeaderComponent]
 })
 export class AppComponent {
 }
