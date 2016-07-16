efb460b39ae2eab4ed6bebf9ae53d21515f96372
Shlomi Kushchi <shlomik@gmail.com>
2016-07-16 10:46:02 +0300

    creating a dummy recipe and embedding it in our page

--- Angular2Course/section3_recipe_book/src/app/recipes/recipe-list/recipe-list.component.ts
+++ Angular2Course/section3_recipe_book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,14 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import {Recipe} from "./recipe";
+import {RecipeItemComponent} from "./recipe-item.component"
 
 @Component({
   moduleId: module.id,
   selector: 'rb-recipe-list',
   templateUrl: 'recipe-list.component.html',
-  styles: []
+  directives: [RecipeItemComponent]
 })
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [];
+  recipe = new Recipe('Dummy', 'Dummy', 'http://www.churchplanting.com/wp-content/uploads/2012/02/dummy-300x195.jpg')
   constructor() {}
 
   ngOnInit() {
--- Angular2Course/section3_recipe_book/src/app/recipes/recipes.component.ts
+++ Angular2Course/section3_recipe_book/src/app/recipes/recipes.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import {RecipeListComponent} from './recipe-list';
 
 @Component({
   moduleId: module.id,
   selector: 'rb-recipes',
   templateUrl: 'recipes.component.html',
-  styles: []
+  directives: [RecipeListComponent]
 })
 export class RecipesComponent implements OnInit {
 
