44a4a2422269d2b5004e75c7340a17a00594a81b
Shlomi Kushchi <shlomik@gmail.com>
2016-07-16 09:55:39 +0300

    data binding to inject the recipe to our componenet
    we want recipe-list to iterate the items and craete on the fly our recipes so te recipe member - recipe
    will be injected in

--- Angular2Course/section3_recipe_book/src/app/recipes/recipe-list/recipe-item.component.ts
+++ Angular2Course/section3_recipe_book/src/app/recipes/recipe-list/recipe-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import {Recipe} from "./recipe"
 
 @Component({
@@ -8,7 +8,7 @@ import {Recipe} from "./recipe"
   styles: []
 })
 export class RecipeItemComponent implements OnInit {
-  recipe: Recipe;
+  @Input() recipe: Recipe;
   recipeId: number;
 
   constructor() {}
