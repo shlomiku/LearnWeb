87af2e44e136215f3eadea775d0ff6f8b5843d14
Shlomi Kushchi <shlomik@gmail.com>
2016-08-13 10:27:54 +0300

    create the recipes component

---
+++ Angular2Course/recipe_book/src/app/recipes/recipes.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-recipes',
+  template: `
+    <p>
+      recipes Works!
+    </p>
+  `,
+  styles: []
+})
+export class RecipesComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
